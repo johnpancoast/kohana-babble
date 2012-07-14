665e83c9a3bd37b60252691454fd8b3d18634777
John Pancoast <shideon@gmail.com>
2012-07-14 16:05:02 -0700

    api response drivers conform to parent class

--- classes/API/Response/Driver/NameValue.php
+++ classes/API/Response/Driver/NameValue.php
@@ -4,7 +4,7 @@
  * name value pair api response
  */
 class API_Response_Driver_NameValue extends API_Response {
-	public function get_encoded_response($response)
+	public function get_encoded_response()
 	{
 		// XXX unimplemented
 		return 'Name Value unimplemented';
--- classes/API/Response/Driver/XML.php
+++ classes/API/Response/Driver/XML.php
@@ -4,7 +4,7 @@
  * xml api response
  */
 class API_Response_Driver_XML extends API_Response {
-	public function get_encoded_response($response)
+	public function get_encoded_response()
 	{
 		// XXX unimplemented
 		return 'XML unimplemented';
