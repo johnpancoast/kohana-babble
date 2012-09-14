209fc87140e40d3f7e452bbaa0b49596c750c42f
John Pancoast <shideon@gmail.com>
2012-09-14 11:38:27 -0700

    bug fix for kohana_request

--- classes/Controller/API.php
+++ classes/Controller/API.php
@@ -56,7 +56,7 @@ class Controller_API extends Controller {
 
 			// gen a new hash from passed data and private key and check against passed hash.
 			// if hashes match then the user has authenticated and we can log them in.
-			$url = URL::base($api_kohana_request).$api_kohana_request->uri().'?'.http_build_query($api_kohana_requet->query());
+			$url = URL::base($api_kohana_request).$api_kohana_request->uri().'?'.http_build_query($api_kohana_request->query());
 			$check_key = API_Request::get_auth_key($user->username, $user->password, $url, $_SERVER['REQUEST_METHOD'], array('resource_data' => $this->api_request->get_resource_data()));
 			if ( ! empty($key) && $key == $check_key)
 			{
