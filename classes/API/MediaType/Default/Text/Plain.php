617378796d070e323acc2aa751e7e029499f5c23
John Pancoast <shideon@gmail.com>
2012-10-13 11:14:25 -0700

    text/plain handles array; added text/html

--- classes/API/MediaType/Default/Text/Plain.php
+++ classes/API/MediaType/Default/Text/Plain.php
@@ -10,10 +10,32 @@ class API_MediaType_Default_Text_Plain extends API_MediaType {
 	protected $media_type = 'text/plain';
 
 	/**
+	 * @var string Key/value separator
+	 * @access protected
+	 */
+	protected $value_sep = ' = ';
+
+	/**
+	 * @var string Set separator
+	 * @access protected
+	 */
+	protected $set_sep = ",\n";
+
+	/**
 	 * @see parent::_get_data_encoded()
 	 */
 	protected function _get_data_encoded($data = array())
 	{
+		// just print key=value pairs separated by comma and new line
+		if (is_array($data))
+		{
+			$ret = array();
+			foreach ($data as $k => $v)
+			{
+				$ret[] = $k.$this->value_sep.$v;
+			}
+			return implode($this->set_sep, $ret);
+		}
 		return $data;
 	}
 
