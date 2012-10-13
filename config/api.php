12bb606dc7301d160f6a63a174bfe6e740fb1a99
John Pancoast <shideon@gmail.com>
2012-10-12 19:32:42 -0700

    api config comments

--- config/api.php
+++ config/api.php
@@ -5,19 +5,59 @@
  */
 return array
 (
-	// the current version of the API. when clients do not pass a version,
-	// this is the version that will be loaded.
+	/**
+	 * [current_version]
+	 *
+	 * the current version of the API. When clients do not pass a version,
+	 * this is the version used where applicable..
+	 */
 	'current_version' => '0.0.1',
 
-	// default media types. You can map a media type to the requested media type
-	// you wish to be loaded.
+	/**
+	 * [default_media_types]
+	 *
+	 * You can map a requested Accept header media type to the media type
+	 * you wish to be loaded.
+	 */
 	'default_media_types' => array(
 		'*/*' => 'application/json',
 	),
 
-	// api testing parameters. {@see Controller_Public_API_Testing}.
-	// typically you'll set these values in your own site config.
-	// {@see application/config/README.md}
+	/**
+	 * [test_user]
+	 *
+	 * !!WARNING!! Be careful with this value.
+	 *
+	 * When test_user is specified and your environment is in Kohana::DEVELOPMENT,
+	 * this user will be automatically logged in as the api user for the request.
+	 * Bypasses security measures when in Kohana::DEVELOPMENT mode _only_ but you
+	 * should still be careful with this setting and your environments.
+	 */
+	'test_user' => NULL,
+
+	/**
+	 * header related values
+	 */
+	'header' => array(
+		/**
+		 * [header.response_header_title]
+		 *
+		 * if this value is specified, a response header of this name will be set.
+		 * it will contain the api response code and message. this can
+		 * be useful to clients.
+		 *
+		 * Be mindful of what you name this header title. Do not use existing
+		 * response header titles. Do not prepend with "X-".
+		 * {@see http://tools.ietf.org/html/rfc6648}
+		 */
+		'response_header_title' => NULL,
+	),
+
+	/**
+	 * [testing]
+	 *
+	 * api testing parameters. {@see Controller_Public_API_Testing}.
+	 */
 	'testing' => array(
 		'user' => NULL,
 		'pass' => NULL,
