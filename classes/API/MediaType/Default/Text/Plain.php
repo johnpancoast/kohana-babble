c0ebae6e53e1a5557f559be62385e28c3a586267
John Pancoast <shideon@gmail.com>
2012-10-12 21:34:55 -0700

    fixed up text/plain media type

--- classes/API/MediaType/Default/Text/Plain.php
+++ classes/API/MediaType/Default/Text/Plain.php
@@ -1,15 +1,27 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
- * text response
+ * media type - text/plain
  */
 class API_MediaType_Default_Text_Plain extends API_MediaType {
 	/**
-	 * @see parent::get_response_encoded()
+	 * @see API_MediaType::media_type
 	 */
-	public function get_response_encoded()
+	protected $media_type = 'text/plain';
+
+	/**
+	 * @see parent::_get_data_encoded()
+	 */
+	protected function _get_data_encoded($data = array())
 	{
-		// we just assume our code has set some text as opposed to an array
-		return $this->get_response();
+		return $data;
+	}
+
+	/**
+	 * @see parent::_get_data_decoded()
+	 */
+	protected function _get_data_decoded($data = NULL)
+	{
+		return $data;
 	}
 }
