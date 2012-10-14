53aa2c976f44faea8b40ce0ec411afea4ac6f3f5
John Pancoast <shideon@gmail.com>
2012-10-14 13:48:29 -0700

    added init routes

---
+++ init.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// public rest api. routes to api frontend.
+// e.g.,
+//     /api/user/get/1
+Route::set('api', 'api/<resource_controller>(/<resource_id>)')
+	->defaults(array(
+		'directory' => 'Public',
+		'controller' => 'API',
+		'action'     => 'index',
+	));
+
+// route for testing above API. only create this is in DEV mode.
+if (Kohana::$environment == Kohana::DEVELOPMENT)
+{
+	Route::set('testapi', '<testapi>', array('testapi' => 'testapi.*'))
+		->defaults(array(
+			'directory' => 'Public/API',
+			'controller' => 'Testing',
+			'action'     => 'index',
+		));
+}
+
