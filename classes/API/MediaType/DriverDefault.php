d9ec68652aa6712f046801f4fb21f83cfa8d0ddf
John Pancoast <shideon@gmail.com>
2012-10-22 13:21:23 -0700

    default media driver class

---
+++ classes/API/MediaType/DriverDefault.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class API_MediaType_DriverDefault extends Babble_API_MediaType_DriverDefault {}
---
+++ classes/Babble/API/MediaType/DriverDefault.php
@@ -0,0 +1,27 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * media type - NULLn
+ */
+class Babble_API_MediaType_DriverDefault extends API_MediaType {
+	/**
+	 * @see API_MediaType::media_type
+	 */
+	protected $media_type = NULL;
+
+	/**
+	 * @see parent::_get_data_encoded()
+	 */
+	protected function _get_data_encoded($data = array())
+	{
+		return $data;
+	}
+
+	/**
+	 * @see parent::_get_data_decoded()
+	 */
+	protected function _get_data_decoded($data = NULL)
+	{
+		return $data;
+	}
+}
