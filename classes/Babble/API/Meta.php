9a9ece32bdfb846a76129b01193456effcb4532b
John Pancoast <shideon@gmail.com>
2012-10-30 08:44:30 -0700

    issue #10 - fixes to meta class

--- classes/Babble/API/Meta.php
+++ classes/Babble/API/Meta.php
@@ -4,30 +4,30 @@
  * api meta data primarily used for logging
  */
 class Babble_API_Meta {
-	private static $request = NULL;
-	private static $response = NULL;
-	private static $header_content_type = array();
-	private static $header_accept = array();
-	private static $version_module_directory = NULL;
+	protected static $request = NULL;
+	protected static $response = NULL;
+	protected static $header_content_type = array();
+	protected static $header_accept = array();
+	protected static $version_module_directory = NULL;
 
-	public static function set_request(API_Request $request)
+	public static function set_request(Babble_API_Request $request)
 	{
-		Babble_API::$request = $request;
+		API_Meta::$request = $request;
 	}
-	public static function set_response(API_Response $response)
+	public static function set_response(Babble_API_Response $response)
 	{
-		Babble_API::$response = $response;
+		API_Meta::$response = $response;
 	}
 	public static function set_header_content_type($media_type, $module_directory)
 	{
-		Babble_API::$header_content_type = array('media_type' => $media_type, 'module_directory' => $module_directory);
+		API_Meta::$header_content_type = array('media_type' => $media_type, 'module_directory' => $module_directory);
 	}
 	public static function set_header_accept($media_type, $module_directory)
 	{
-		Babble_API::$header_accept = array('media_type' => $media_type, 'module_directory' => $module_directory);
+		API_Meta::$header_accept = array('media_type' => $media_type, 'module_directory' => $module_directory);
 	}
 	public static function set_version_module_directory($version_module_directory)
 	{
-		Babble_API::$version_module_directory = $version_module_directory;
+		API_Meta::$version_module_directory = $version_module_directory;
 	}
 }
