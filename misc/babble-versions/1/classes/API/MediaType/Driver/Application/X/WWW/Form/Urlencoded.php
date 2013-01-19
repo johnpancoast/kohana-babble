2e3de8ca562459a865d8c3a20b40a594fe273673
John Pancoast <shideon@gmail.com>
2013-01-19 10:53:57 -0800

    issue #21 - babble versions contains transparent class defaults

--- misc/babble-versions/1/classes/API/MediaType/Driver/Application/X/WWW/Form/Urlencoded.php
+++ misc/babble-versions/1/classes/API/MediaType/Driver/Application/X/WWW/Form/Urlencoded.php
@@ -1,29 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-/**
- * media type - application/x-www-form-urlencoded
- */
-class API_MediaType_Driver_Application_X_WWW_Form_Urlencoded extends Babble_API_MediaType_Driver_Application_X_WWW_Form_Urlencoded {
-	/**
-	 * @see API_MediaType::media_type
-	 */
-	protected $media_type = 'application/x-www-form-urlencoded';
-
-	/**
-	 * @see parent::_get_encoded_resource()
-	 */
-	protected function _get_encoded_resource(Babble_API_Resource $resource)
-	{
-		return http_build_query($data->as_array());
-	}
-
-	/**
-	 * @see parent::_get_decoded_resource()
-	 */
-	protected function _get_decoded_resource($data = NULL)
-	{
-		parse_str($data, $input);
-		$data = isset($input) ? (array)$input : array();
-		return new API_Resource($data);
-	}
-}
+class API_MediaType_Driver_Application_X_WWW_Form_Urlencoded extends Babble_API_MediaType_Driver_Application_X_WWW_Form_Urlencoded {}
--- misc/babble-versions/1/classes/API/MediaType/Driver/Text/Html.php
+++ misc/babble-versions/1/classes/API/MediaType/Driver/Text/Html.php
@@ -1,20 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-/**
- * media type - text/html
- *
- * similar handling to text/plain, so extend it
- */
-class API_MediaType_Driver_Text_Html extends Babble_API_MediaType_Driver_Text_Html {
-	/**
-	 * @see API_MediaType::media_type
-	 * @access protected
-	 */
-	protected $media_type = 'text/html';
-
-	/**
-	 * @var string Set separator
-	 * @access protected
-	 */
-	protected $set_sep = "<br/>\n";
-}
+class API_MediaType_Driver_Text_Html extends Babble_API_MediaType_Driver_Text_Html {}
