51a975533c8731f7a82246b3140630afcddfec49
John Pancoast <shideon@gmail.com>
2013-01-19 10:47:55 -0800

    removed unused index controller

--- classes/Babble/Controller/Public/Index.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Babble_Controller_Public_Index extends Controller {
-
-	public function action_index()
-	{
-		$this->response->body('hello, world!');
-	}
-}
--- classes/Controller/Public/Index.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Public_Index extends Babble_Controller_Public_Index {}
